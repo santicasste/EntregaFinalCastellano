c51ad12b531466d2d7a88f18040ad1cf621a18c4
Santiago Castellano <santicastellano04@gmail.com>
2024-05-17 16:28:32 -0300

    final

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-class Producto {  // Creo una clase de productos para luego ahorrar tiempo y espacio al momento de crear uno.
-    constructor(nombre, precio, tipo) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.tipo = tipo;
-    }
-}
 
-//! Vinilos
-let vida = new Producto("Vida", 58399, "Vinilo");
-// let peliculas = new Producto("Películas", 52399, "Vinilo");
-// let bicicleta = new Producto("Bicicleta", 63199, "Vinilo");
-let pubis = new Producto("Pubis Angelical", 58199, "Vinilo");
 
-//! Indumentaria 
-let remera = new Producto("Remera Clics Modernos", 30000, "Indumentaria");
-// let mochila = new Producto("Mochila Serú Girán", 52399, "Indumentaria");
-// let campera = new Producto("Campera Piano Bar", 20199, "Indumentaria");
-let pantalon = new Producto("Pantalón Say No More", 51990, "Indumentaria");
 
-//! Instrumentos
-let piano = new Producto("Piano Yamaha CP-70", 12000000, "Instrumento");
-// let sintetizador = new Producto("Sintetizador Roland Jupiter-6", 1200000, "Instrumento");
-// let caja = new Producto("Caja de ritmos TR-808", 900200, "Instrumento");
-let guitarra = new Producto("Guitarra eléctrica Rickenbacker 365", 873000, "Instrumento");
-
-const productos = [vida, pubis, remera, pantalon, piano, guitarra];
+fetch("/data/productos.json")
+    .then(res => res.json()) 
+    .then(data => {
+        data.forEach((producto) => {   // Recorre el array de prodcutos y los va mostrando en la página 
+            const div = document.createElement("div"); // Creo el elemento "div" que representa al div del HTML
+            div.classList.add("producto"); // Le agrego la clase "producto" al div
+        
+            div.innerHTML = ` 
+                <img src="${producto.img}">
+                <h3 class="producto_data">${producto.nombre}</h3>
+                <p class="producto_data">$${producto.precio}</p>
+            `; // Junto al .innerHTML voy poniendo que quiero que aparezca en la web
+        
+            const btn = document.createElement("button"); // Creo el elemento "btn" en representacion a un boton de HTML
+            btn.classList.add("btn_carrito"); // Le agrego la clase "btn_carrito"
+            btn.innerText = "Agregar al carrito";  // Junto al .innerText le pongo el texto que quiero que muestre 
+            btn.addEventListener("click", () =>{ // Le agrego el evento deseado
+                agregarAlCarrito(producto);
+                
+                console.log(carrito);
+            });
+            div.append(btn); // Junto a .append lo pongo en el "div"
+            contenedorProductos.append(div); // Junto a .append lo pongo junto al contenedorProductos
+        });
+    })
 
 //  Funciones
 const contenedorCarrito = document.querySelector(".container_carrito");
@@ -38,7 +39,11 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || []; //Si hay algo e
 
 function actualizarTotal(){
     const montoTotal = carrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
-    totalCarrito.innerText = `$${montoTotal}`;
+    totalCarrito.innerText = `Total: $${montoTotal}`;
+    
+    if(montoTotal === 0){
+        totalCarrito.innerText = "-";
+    }
 }
 
 
@@ -59,14 +64,16 @@ function borrarDelCarrito(producto){
 function actualizarCarrito(){
     contenedorCarrito.innerHTML = ""; // Limpiamos el contenido existente antes de volver a renderizar
 
-    if (carrito.length !== 0){  // Valida
+    if (carrito.length === 0){
+        contenedorCarrito.innerText = "No hay productos en su carrito... 🙁"
+     } else { // Valida
         carrito.forEach((producto) => {  // Recorre el carrito y crea un div con su respectivo texto e informacion para cada uno
             const div = document.createElement("div"); 
             div.classList.add("carrito_producto") 
             div.innerHTML = `  
             <h2>${producto.nombre}</h2>
-            <p>$${producto.precio}</p>
-            <p>${producto.cantidad}</p>
+            <p>Precio: $${producto.precio}</p>
+            <p>Cantidad: ${producto.cantidad}</p>
             <p>Subt: $${producto.cantidad * producto.precio}</p>
             `; 
 
@@ -75,7 +82,18 @@ function actualizarCarrito(){
             btn.innerText = "Borrar ❌";
             btn.addEventListener("click", () => {
                 borrarDelCarrito(producto);
-                console.log("Producto borrado");
+                Toastify({
+                    text: "Producto eliminado",
+                    duration: 1000,
+                    close: true,
+                    gravity: "bottom", // `top` or `bottom`
+                    position: "right", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                    background: "linear-gradient(to right, #840705, #2e0f15)",
+                    },
+                    onClick: function(){} // Callback after click
+                }).showToast();
             });
             div.append(btn);
             contenedorCarrito.appendChild(div); // Utilizamos appendChild() para agregar el elemento al DOM
@@ -98,28 +116,19 @@ function agregarAlCarrito(producto){ // Funcion para sumar la cantidad en caso d
     }
     actualizarCarrito();
     localStorage.setItem("carrito", JSON.stringify(carrito));
+    Toastify({
+        text: "Producto agregado ✔",
+        duration: 1000,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+        background: "linear-gradient(to right, #2e0f15, #840705)",
+        },
+        onClick: function(){} // Callback after click
+    }).showToast();
 }
 
 
-productos.forEach((producto) => {   // Recorre el array de prodcutos y los va mostrando en la página 
-    const div = document.createElement("div"); // Creo el elemento "div" que representa al div del HTML
-    div.classList.add("producto"); // Le agrego la clase "producto" al div
-
-    div.innerHTML = ` 
-        <img src="../images/pubis.jpg" alt="pubis_angelical">
-        <h3 class="producto_data">${producto.nombre}</h3>
-        <p class="producto_data">$${producto.precio}</p>
-    `; // Junto al .innerHTML voy poniendo que quiero que aparezca en la web
-
-    const btn = document.createElement("button"); // Creo el elemento "btn" en representacion a un boton de HTML
-    btn.classList.add("btn_carrito"); // Le agrego la clase "btn_carrito"
-    btn.innerText = "Agregar al carrito";  // Junto al .innerTexto le pongo el texto que quiero que muestre 
-    btn.addEventListener("click", () =>{ // Le agrego el evento deseado
-        agregarAlCarrito(producto);
-        
-        console.log(carrito);
-    });
-    div.append(btn); // Junto a .append lo pongo en el "div"
-    contenedorProductos.append(div); // Junto a .append lo pongo junto al contenedorProductos
-})
 
